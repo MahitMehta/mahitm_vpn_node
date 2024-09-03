c9780ab420ecabd9af2dd180fc8844a1ff693ef6
Mahit Mehta <mahit.py@gmail.com>
2024-09-03 02:52:21 +0000

    Add/Remove Peer

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,25 +1,28 @@
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 use std::fs;
 
 pub const CONFIG_FILENAME: &'static str = "config.toml";
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Node {
     pub id: String,
     pub ipv4: String,
     pub src_port: u16,
     pub dst_port: u16,
     pub network_adapter: String,
+    pub wg_interface: String,
     pub conf_path: String,
+    pub private_key: Option<String>,
+    pub public_key: Option<String>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ControlPlane {
     pub host: String,
     pub secure: bool,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Config {
     pub node: Node,
     pub control_plane: ControlPlane,
@@ -43,5 +46,13 @@ impl Config {
         };
 
         Ok(config)
+    }
+
+    pub fn update_keys(&mut self, private_key: String, public_key: String) {
+        self.node.private_key = Some(private_key);
+        self.node.public_key = Some(public_key);
+
+        let contents = toml::to_string(&self).unwrap();
+        fs::write(CONFIG_FILENAME, contents).unwrap();
     }
 }
